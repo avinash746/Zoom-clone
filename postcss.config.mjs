d0a1045223071c68f40d82c8b0dc27fbe5d09195
avinash746 <anish95380@gmail.com>
2024-10-06 02:05:41 +0530

    first commit

---
+++ postcss.config.mjs
@@ -0,0 +1,20 @@
+// /** @type {import('postcss-load-config').Config} */
+// const config = {
+//   plugins: {
+//     tailwindcss: {},
+//   },
+// };
+
+// export default config;
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
